2ac22867efdf354377b6a4b50480dc33a86487fa
Nikolay Ivanov <nikolay.ivanov@accenture.com>
2017-10-03 10:33:28 +0300

    palindrome Kata

---
+++ kata/palindrome.js
@@ -0,0 +1,21 @@
+exports.isPalindrome = function isPalindrome(aString) {
+    // make sure we are dealing with a valid value for aString.
+    if (typeof aString !== 'number' && typeof aString !== 'string') {
+        return false;
+    }
+
+    if (typeof aString === 'number') {
+        aString = aString.toString();
+    }
+
+    // anything that is not a through z ( case insensitive and globally ) and 0 through 9 remove it. then do a lower case.
+    const cleanString = aString.replace(/[^a-z0-9]/gi, '').toLowerCase();
+
+    // using two vars i and j and going towards the center of the string. this algorithm is not efficient because it overlaps. Big O (N)
+    for (let i = cleanString.length - 1, j = 0; i > 0 && j < cleanString.length; i -= 1, j += 1) {
+        if (cleanString.charAt(i) !== cleanString.charAt(j)) {
+            return false;
+        }
+    }
+    return true;
+};
---
+++ test/palindromeTest.js
@@ -0,0 +1,89 @@
+const assert = require('chai').assert;
+const isPalindrome = require('../kata/palindrome').isPalindrome;
+
+describe('Palindromes', () => {
+    context('passing non-valid values', () => {
+        describe('Given I have passed undefined', () => {
+            it('should return false', () => {
+                const result = isPalindrome();
+                assert.equal(result, false);
+            });
+        });
+        describe('Given I have passed null', () => {
+            it('should return false', () => {
+                const result = isPalindrome(null);
+                assert.equal(result, false);
+            });
+        });
+        describe('Given I have passed a numeric int ', () => {
+            it('should return false', () => {
+                const result = isPalindrome(1245);
+                assert.equal(result, false);
+            });
+        });
+        describe('Given I have passed a numeric real', () => {
+            it('should return false', () => {
+                const result = isPalindrome(12.45);
+                assert.equal(result, false);
+            });
+        });
+        describe('Given I have passed a numeric negative', () => {
+            it('should return false', () => {
+                const result = isPalindrome(-1234);
+                assert.equal(result, false);
+            });
+        });
+    });
+    describe('Given I am passing strings', () => {
+        describe('Given I have passed a blank', () => {
+            it('should return true', () => {
+                const result = isPalindrome('');
+                assert.equal(result, true);
+            });
+        });
+        it('should return true if a word is a palindrome', () => {
+            const result = isPalindrome('abba');
+            assert.equal(result, true);
+        });
+        describe('Given I have passed a simple symmetric value', () => {
+            it('should return true', () => {
+                const result = isPalindrome('ABBA');
+                assert.equal(result, true);
+            });
+        });
+        describe('Given I have passed a simple asymmetric value', () => {
+            it('should return true', () => {
+                const result = isPalindrome('ABCBA');
+                assert.equal(result, true);
+            });
+        });
+        describe('Given I have passed a simple symmetric mix value', () => {
+            it('should return true', () => {
+                const result = isPalindrome('ABba');
+                assert.equal(result, true);
+            });
+        });
+        describe('Given I have passed a simple asymmetric mix value', () => {
+            it('should return true', () => {
+                const result = isPalindrome('ABCba');
+                assert.equal(result, true);
+            });
+        });
+        it('should return false when it is not a Palindrome', () => {
+            const result = isPalindrome('The quick brown fox jumps over the lazy dog');
+            assert.equal(result, false);
+        });
+        it('should return true when it is Palindrome of lower case', () => {
+            const result = isPalindrome('a toyotas a toyota');
+            assert.equal(result, true);
+        });
+        it('should return true when it is Palindrome of mixed case', () => {
+            const result = isPalindrome('a tOyoTaS A toYota');
+            assert.equal(result, true);
+        });
+        it('should return true when it is Palindrome of mixed case with punctuation', () => {
+            const result = isPalindrome('A Toyota’s a Toyota.');
+            assert.equal(result, true);
+        });
+    });
+});
