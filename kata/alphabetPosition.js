c05c22300ec63b5b310d80f374f1c3c615f675aa
Nikolay Ivanov <nikolay.ivanov@accenture.com>
2017-10-30 14:34:21 +0200

    alphabetPosition Kata added

---
+++ kata/alphabetPosition.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} text - input string to be converted into numbers
+ * @returns {string} - result containing the numbers of the order of the letters in the alphabet
+ */
+function alphabetPosition(text) {
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const array = [];
+    text.toLowerCase().split('').map((letter) => {
+        alphabet.map((value, key) => {
+            if (letter === value) {
+                array.push(key + 1);
+            }
+        });
+    });
+    text = array.join(' ');
+    return text;
+}
+
+alphabetPosition('The narwhal bacons at midnight.');
+
+module.exports = {
+    alphabetPosition
+};
---
+++ test/alphabetPositionTest.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const alphabetPosition = require('../kata/alphabetPosition').alphabetPosition;
+
+describe('Phrases that should be equal', () => {
+    it('The narwhal bacons at midnight.', () => {
+        assert.equal(alphabetPosition('The narwhal bacons at midnight.'), '20 8 5 14 1 18 23 8 1 12 2 1 3 15 14 19 1 20 13 9 4 14 9 7 8 20');
+    });
+
+    it('The sunset sets at twelve o\' clock.', () => {
+        assert.equal(alphabetPosition('The sunset sets at twelve o\' clock.'), '20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11');
+    });
+});
